6be0530314797a82d5a062bfe41d556413693f93
suneo3476 <suneo3476@gmail.com>
2017-04-23 19:37:22 +0900

    Create PostformBeforeLTL.js
    Tampermonkeyで編集したものをそのまま貼った

---
+++ PostformBeforeLTL.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Postform before LTL
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://friends.nico/web/timelines/public/local
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    const HOME = 0,
+             LTL = 2;
+    const postform = document.querySelector( ".drawer__inner div:nth-child(3)" );
+    const ltl = document.querySelectorAll('.column-header')[LTL];
+    ltl.parentNode.insertBefore(postform, ltl);
+})();
